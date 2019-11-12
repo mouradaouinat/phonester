96d99983308807ef03404b4e3befafdac2e72427
Mourad Aouinat <mourad.eyes@gmail.com>
2019-11-12 10:52:01 +0100

    first major refactoring to the navbar components, I added a red badge instead of the old one, also installed 'styled Components' and used it on the Navbar component! Let's keep it rolling.

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
 const Checkout = ({ cartItems, onClear }) => {
   const total = cartItems.reduce((acc, item) => {
     return acc + item.price * item.count;
   }, 0);
-  const tax = total * 0.2;
-  const orderTotal = total + tax;
+  const tax = (total * 0.2).toFixed(2);
+  const orderTotal = total + parseFloat(tax);
   return (
     <div className="checkout-card">
       <div className="in-between mb-2">
         <span>Total:</span>
-        <span className="grey">${total}</span>
+        <span className="grey">${total.toFixed(2)}</span>
       </div>
       <div className="in-between mb-2">
         <span>Tax:</span>
@@ -18,7 +18,7 @@ const Checkout = ({ cartItems, onClear }) => {
       </div>
       <div className="in-between mb-2">
         <span>Order total:</span>
-        <span className="grey">${orderTotal}</span>
+        <span className="grey">${orderTotal.toFixed(2)}</span>
       </div>
       <div className="in-between">
         <button className="btn-secondary">Checkout</button>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,42 +1,155 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
 
-const Navbar = ({ inCart }) => {
-  return (
-    <nav>
-      <div className="navbar">
-        <div className="inner container">
-          <div className="logo">
-            <Link to="/products">phonester</Link>
-          </div>
-          <div className="navbar-elements">
-            <div className="search">
-              <input
-                type="text"
-                name="search"
-                className="search-input"
-                placeholder="Search"
-              />
-              <button className="search-button">
+const Logo = styled.div`
+  font-family: "Maven Pro", sans-serif;
+  font-weight: 900;
+  font-size: 20px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 8vh;
+  background-color: #050024;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  margin-bottom: 200px;
+`;
+
+const NavElements = styled.ul`
+  display: flex;
+  width: auto;
+  justify-content: space-around;
+  align-items: center;
+
+  @media screen and (max-width: 900px) {
+    width: 40%;
+  }
+
+  @media screen and (max-width: 768px) {
+    position: absolute;
+    right: 0px;
+    top: 8vh;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 60%;
+    height: 92vh;
+    background-color: #050024;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    transition: all 0.3s ease;
+  }
+`;
+
+const ListItems = styled.li`
+  list-style: none;
+  position: relative;
+`;
+
+const Search = styled.div`
+  display: inline-block;
+  border: 1px solid black;
+  padding: 5px 10px;
+  border-radius: 20px;
+  clear: both;
+  margin-right: 8px;
+  background: #ffffff;
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  font-size: 16px;
+  display: inline;
+  width: 100px;
+`;
+
+const SearchButton = styled.button`
+  background: transparent;
+  border: none;
+  font-size: 16px;
+  color: rgb(78, 78, 78);
+  display: inline;
+`;
+
+const RedBadge = styled.span`
+  border-radius: 30px;
+  color: #fff;
+  font-size: 10px;
+  background-color: #ff5656;
+  border: none;
+  padding: 0 3px;
+  position: absolute;
+  left: 45px;
+  top: -3px;
+`;
+
+const Burger = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    cursor: pointer;
+  }
+`;
+
+const BurgerLine = styled.div`
+  width: 20px;
+  height: 2px;
+  background-color: cornsilk;
+  margin: 4px;
+  border-radius: 10px;
+`;
+
+class Navbar extends Component {
+  state = {
+    open: false
+  };
+
+  handleToggle = () => {
+    let open = this.state.open;
+    open = !open;
+    this.setState({ open });
+  };
+
+  render() {
+    const { inCart } = this.props;
+    return (
+      <Nav>
+        <Logo>
+          <Link to="/products">phonester</Link>
+        </Logo>
+        <NavElements open={this.state.open}>
+          <ListItems>
+            <Search>
+              <SearchInput type="text" name="search" placeholder="Search" />
+              <SearchButton>
                 <i className="fa fa-search"></i>
-              </button>
-            </div>
-            <ul className="products-nav">
-              <li className="link-item">
-                <Link to="/products">Products</Link>
-              </li>
-              <li className="link-item">
-                <Link to="/cart">
-                  <i className="fa fa-shopping-cart"></i>
-                </Link>
-                <span className="badge">{inCart.length}</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+              </SearchButton>
+            </Search>
+          </ListItems>
+          <ListItems>
+            <Link to="/products">Products</Link>
+          </ListItems>
+          <ListItems>
+            <Link to="/cart">
+              Cart <i className="fa fa-shopping-cart"></i>
+            </Link>
+            {inCart.length !== 0 && <RedBadge>{inCart.length}</RedBadge>}
+          </ListItems>
+        </NavElements>
+        <Burger onClick={this.handleToggle}>
+          <BurgerLine></BurgerLine>
+          <BurgerLine></BurgerLine>
+          <BurgerLine></BurgerLine>
+        </Burger>
+      </Nav>
+    );
+  }
+}
 
 export default Navbar;
